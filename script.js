9cb0815fca5ad05e3f4adfb94a0b91b44c892c87
Tian <Tian@Louises-iMac-2.local>
2017-12-14 21:38:05 +1300

    adding api html, css and js

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', start);
+
+var tereoAPI = "https://eda-te-reo.herokuapp.com/api/proverbs";
+var proverb;
+
+function start () {
+
+
+}
+
+function proverbMaker () {
+  $.getJSON( tereoAPI, {
+    tags: "any",
+    tagmode: "any",
+    format: "json"
+  })
+    .done(function( data ) {
+      proverb = data["source"];
+      $("#proverb").html("Proverb: " + (data["source"]));
+      $("#translation").html("Translation: " + (data["translation"]));
+    });
+}
+
+function tweeter() {
+  window.location.href=("https://twitter.com/intent/tweet?text=" + proverb);
+}
