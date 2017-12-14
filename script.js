500035211221605ae27cb33b264816e7f2d0fe5b
Tian <Tian@Louises-iMac-2.local>
2017-12-14 22:34:55 +1300

    changed to .ajax from .getJSON

--- script.js
+++ script.js
@@ -4,22 +4,38 @@ var tereoAPI = "https://eda-te-reo.herokuapp.com/api/proverbs";
 var proverb;
 
 function start () {
-
-
 }
 
+
 function proverbMaker () {
-  $.getJSON( tereoAPI, {
-    tags: "any",
-    tagmode: "any",
-    format: "json"
-  })
-    .done(function( data ) {
+  $.ajax({
+     url: "https://eda-te-reo.herokuapp.com/api/proverbs",
+     error: function() {
+        $('#proverb').html('<p>An error has occurred</p>');
+        },
+    dataType: 'json',
+    success: function(data){
       proverb = data["source"];
       $("#proverb").html("Proverb: " + (data["source"]));
       $("#translation").html("Translation: " + (data["translation"]));
-    });
+    },
+    type: 'GET'
+  });
 }
+
+
+// function proverbMaker () {
+//   $.getJSON( tereoAPI, {
+//     tags: "any",
+//     tagmode: "any",
+//     format: "json"
+//   })
+//     .done(function( data ) {
+//       proverb = data["source"];
+//       $("#proverb").html("Proverb: " + (data["source"]));
+//       $("#translation").html("Translation: " + (data["translation"]));
+//     });
+// }
 
 function tweeter() {
   window.location.href=("https://twitter.com/intent/tweet?text=" + proverb);
